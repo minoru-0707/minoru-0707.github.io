8dc9773fc075e56ec79cdfc4f81f74662e04df99
minoru <minoru4194304@gmail.com>
2026-07-11 17:34:15 +0900

    released-versin.1.0

---
+++ assets/js/parallax.js
@@ -0,0 +1,23 @@
+(function () {
+    const wrapper = document.querySelector('.parallax-wrapper');
+    if (!wrapper) return;
+
+    let ticking = false;
+
+    function updateParallax() {
+    const scrollY = window.scrollY || window.pageYOffset || 0;
+    const offset = scrollY * 0.5;
+    wrapper.style.backgroundPosition = `center ${-offset}px`;
+    ticking = false;
+    }
+
+    function requestTick() {
+    if (!ticking) {
+        window.requestAnimationFrame(updateParallax);
+        ticking = true;
+    }
+    }
+
+    window.addEventListener('scroll', requestTick, { passive: true });
+    updateParallax();
+})();
